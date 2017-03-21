313b3f691493c54d034ae2315d554b2b1439f1b8
Ahmed Yousry <Ahmed Yousry>
2017-03-21 20:18:13 +0200

    create and new message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'ahmed',
-    text: 'you. fuck off!!'
-  });
 });
 
 socket.on('disconnect', function () {
