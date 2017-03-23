2db84ecb605a699d23854a1cfef1e48e33567ac1
Ahmed Yousry <Ahmed Yousry>
2017-03-23 02:01:28 +0200

    users class& usersUpdate list

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,10 @@
 var socket = io();
 
 function scrollToBottom () {
-  // Selectors
+  // S
   var messages = jQuery('#messages');
   var newMessage = messages.children('li:last-child')
-  // Heights
+  // H
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
@@ -17,11 +17,30 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
